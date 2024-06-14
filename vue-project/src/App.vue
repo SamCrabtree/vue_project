56eda4ba0912479fe9ccadc06f9e772579da2e1a
Sam Crabtree <samuel.crabtree@protonmail.com>
2024-06-14 09:11:38 -0400

    Initial Setup from Vue.JS and VueSchool Getting Started

---
+++ vue-project/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello World!')
+</script>
+
+<template>
+  <pre>
+    <h1>Vue Testing Ground</h1>
+    <h2>{{ msg }}</h2>
+    <input v-model="msg" />
+  </pre>
+</template>
